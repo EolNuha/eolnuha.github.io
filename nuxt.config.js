06612add3f1df1379400653d53053f145b9938a5
eol nuha <olinuha08@hotmail.com>
2023-03-08 09:09:58 +0100

    added robots

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,7 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { name: "format-detection", content: "telephone=no" },
+      { name: "robots", content: "index, follow" },
       {
         name: "description",
         content:
