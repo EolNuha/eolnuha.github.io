8b51380d097f57ba5c3b415f953cee438b871028
Eol Nuha <Eol Nuha>
2026-03-20 19:12:32 +0100

    chore: update profile page

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "EOL NUHA",
-  profile: '/profile.jpg',
+  profile: '/profile.PNG',
   designation: "Software Developer",
   description: "As a highly skilled Frontend Developer at Gjirafa, I specialize in utilizing my problem-solving abilities and technical expertise to create innovative and efficient solutions. With a strong background in various programming languages, including Python, JavaScript, TypeScript, Java, React.js, and Vue.js, I am well-equipped to tackle any challenge that arises. My passion for technology, combined with my independent, introverted nature, allows me to excel in a fast-paced and dynamic work environment. I am always seeking to expand my skill set and connect with other professionals in the software engineering field.",
   email: 'eol.nuha22@gmail.com',
